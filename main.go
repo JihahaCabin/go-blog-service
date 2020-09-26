7eba36342e8278256d2c66b737d86add968e0617
zhengzhendu <zdzhengzhendu@163.com>
2020-09-26 14:44:34 +0800

    使用viper，读取配置文件

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-programming-tour-book/blog-service/global"
 	"github.com/go-programming-tour-book/blog-service/internal/routers"
+	setting "github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
 
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	fmt.Printf("%#v\n", global.ServerSetting)
+	fmt.Printf("%#v\n", global.DatabaseSetting)
+	fmt.Printf("%#v\n", global.AppSetting)
+
+	return nil
+}
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
